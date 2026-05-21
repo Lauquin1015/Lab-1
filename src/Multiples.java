db43ad80fe56ea33b896859254429ba214fab893
Laura Q <laura.quintero.cor@gmail.com>
2026-05-21 17:27:50 -0400

    FINAL

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
-
 public class Multiples {
+    public static void main(String[] args) {
+        int count = 0;
+        int x = 1;
+
+        while (x < 1000) {
+            boolean divisibleBy3 = x % 3 == 0;
+            boolean divisibleBy5 = x % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                count += 1;
+            }
+            x++;
+        }
+        System.out.println(count);
+    }
 }
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int n = 100;
+        int count = 0;
+
+        while (n > 0) {
+            count += 1;
+            boolean isEven = n % 2 == 0;
+            boolean isOdd = n % 2 != 0;
+
+            if (isEven) {
+                n = n/2;
+            }
+            else {
+                n -= 1;
+            }
+        }
+        System.out.println(count);
+    }
 }
